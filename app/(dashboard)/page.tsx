1e0a9cfea6e83700c1f1413d308a018c391db840
radubuzoianu <radubuzoianu@yahoo.com>
2025-03-03 17:46:53 +0200

    fixed page.tsx props error

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -6,6 +6,6 @@ export default async function ProductsPage(
   }
 ) {
     return (
-      <div><Button>Click me</Button>Hello World! {searchParams.query}</div>
+      <div><Button>Click me</Button>Hello World! {(await props.searchParams).q}</div>
   );
 }
